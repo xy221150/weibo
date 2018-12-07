38e21f03216c9ce9bac1fa2c29da3a63465c1fbc
daybreak <xy221150@gmail.com>
2018-12-07 14:50:59 +0800

    更新

--- app/src/main/java/org/wxy/weibo/cosmos/network/api/IFriendships.java
+++ app/src/main/java/org/wxy/weibo/cosmos/network/api/IFriendships.java
@@ -7,10 +7,12 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface IFriendships {
+    //关注列表
     @GET("2/friendships/friends.json")
     Call<Friendshipsbean> friend(@Query("access_token")String access_token,
                                  @Query("uid")String uid,
                                  @Query("cursor")int cursor);
+    //粉丝列表
     @GET("2/friendships/followers.json")
     Call<Friendshipsbean> followers(@Query("access_token")String access_token,
                                     @Query("uid")String uid);
