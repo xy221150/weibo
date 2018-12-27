de6a9ff7ede7e34018d948c032c88af8f6a17b3a
daybreak <xy221150@gmail.com>
2018-12-27 15:39:07 +0800

    更新

--- app/src/main/java/org/wxy/weibo/cosmos/ui/activity/MainActivity.java
+++ app/src/main/java/org/wxy/weibo/cosmos/ui/activity/MainActivity.java
@@ -62,24 +62,6 @@ public class MainActivity extends WidgetActivity
     private Userbean userbean;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            // 透明状态栏
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-            getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-    }
-
-    @Override
     protected int getLayoutID() {
         return R.layout.activity_main;
     }
