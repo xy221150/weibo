00fba14e98e6d11ac92150f196d7b752b0e3434b
daybreak <xy221150@gmail.com>
2018-12-29 12:07:01 +0800

    更新

--- app/src/main/java/org/wxy/weibo/cosmos/network/RetrofitHelper.java
+++ app/src/main/java/org/wxy/weibo/cosmos/network/RetrofitHelper.java
@@ -3,20 +3,11 @@ package org.wxy.weibo.cosmos.network;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.wxy.weibo.cosmos.Constants;
-import org.wxy.weibo.cosmos.network.json.JsonItem;
-import org.wxy.weibo.cosmos.network.json.JsonItemArray;
 
-import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
-import okhttp3.RequestBody;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -68,103 +59,4 @@ public class RetrofitHelper {
         return getRetrofitHelper().getmRetrofit().create(cls);
     }
 
-    public  static RequestBody getBody(String content){
-        return RequestBody.create(MediaType.parse("application/json"),content);
-    }
-    public  static RequestBody getBody(JsonItem...jsonItems){
-        JSONObject jo = new JSONObject();
-        if (null != jsonItems && jsonItems.length>0){
-            for (JsonItem jsonItem : jsonItems){
-                try {
-                    if (jsonItem == null){
-                        continue;
-                    }
-                    jo.put(jsonItem.getKey(),jsonItem.getValue());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return  RequestBody.create(MediaType.parse("application/json"),jo.toString());
-    }
-    public static RequestBody getBody(JsonItemArray itemArray, JsonItem... jsonItemArray) {
-        JSONObject jsonObject = new JSONObject();
-
-        if (itemArray == null) {
-            return getBody(jsonItemArray);
-        }
-        String key = itemArray.getKey();
-        String[] strArray = itemArray.getStrList();
-        JsonItem[][] jsonItemArrays = itemArray.getJsonItems();
-        JSONArray jsonArray = new JSONArray();
-
-
-        if (strArray != null && strArray.length > 0) {
-            for (String item : strArray) {
-                if (item == null) {
-                    continue;
-                }
-                jsonArray.put(item);
-            }
-            try {
-                jsonObject.put(key, jsonArray);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        } else if (jsonItemArrays != null && jsonItemArrays.length > 0) {
-            try {
-                for (JsonItem[] jsonItems : jsonItemArrays) {
-                    JSONObject itemObject = new JSONObject();
-                    for (JsonItem item : jsonItems) {
-                        if (item == null) {
-                            continue;
-                        }
-                        itemObject.put(item.getKey(), item.getValue());
-                    }
-                    jsonArray.put(itemObject);
-
-                }
-                jsonObject.put(key, jsonArray);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (jsonItemArray != null && jsonItemArray.length > 0) {
-            try {
-                for (JsonItem item : jsonItemArray) {
-                    if (item == null) {
-                        continue;
-                    }
-                    jsonObject.put(item.getKey(), item.getValue());
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return RequestBody.create(MediaType.parse("application/json"), jsonObject.toString());
-    }
-
-   public static MultipartBody.Part getbody(File file){
-       MultipartBody.Part part=null;
-        if (file!=null) {
-            RequestBody body = RequestBody.create(MediaType.parse("multipart/from-data"), file);
-            part = MultipartBody.Part.createFormData("flie", file.getName(), body);
-        }
-        else
-            part=MultipartBody.Part.createFormData("flie","");
-       return part;
-   }
-    public static MultipartBody.Part getbody(String pic){
-        MultipartBody.Part part=null;
-        if (pic!=null) {
-            RequestBody body = RequestBody.create(MediaType.parse("multipart/from-data"), pic);
-            part = MultipartBody.Part.createFormData("pic", pic, body);
-        }
-        else
-            part=MultipartBody.Part.createFormData("pic","");
-        return part;
-    }
 }
